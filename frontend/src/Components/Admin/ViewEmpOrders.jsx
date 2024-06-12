7d923d7b8b1978f00d29e6d5f48d6f643b16cd83
vThenujan <vthenujan7400@gmail.com>
2024-06-12 15:59:42 +0530

    employee allowance update

--- frontend/src/Components/Admin/ViewEmpOrders.jsx
+++ frontend/src/Components/Admin/ViewEmpOrders.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import { retrieveToken } from '../../Services/JwtToken';
-import { getOrders } from '../../Services/EmployeeService';
+import { getAllowanceDetails, getOrders, updateAllowanceStatus } from '../../Services/EmployeeService';
 import AdminVerticalNav from './AdminVerticalNav';
+import { getAllowance } from '../../Services/AdminServices';
+import { all } from 'axios';
 
 const ViewEmpOrders = () => {
     const navigate = useNavigate();
@@ -19,20 +21,29 @@ const ViewEmpOrders = () => {
 
     const { emp_id } = useParams();
     const [orders, setOrders] = useState([]);
+    const [allowanceDetails, setAllowanceDetails] = useState([])
 
     useEffect(() => {
         const fetchOrders = async (emp_id) => {
             try {
                 const response = await getOrders(emp_id);
-                console.log('res : ', response.data);
                 setOrders(response.data);
+
+                const allowanceDetailsPromises = response && response.data.map(async (order) => {
+                    const allowanceResponse = await getAllowanceDetails(emp_id, order.order_id)
+                    return allowanceResponse.data
+                })
+                console.log('allo :', allowanceDetailsPromises)
+ 
+                const allAllowanceDetails = await Promise.all(allowanceDetailsPromises);
+                setAllowanceDetails(allAllowanceDetails.flat());
+
             } catch (error) {
                 console.log('error occur :', error);
             }
         };
         fetchOrders(emp_id);
     }, [emp_id]);
-
     const addClassName = (status) => {
         switch (status) {
             case 'completed':
@@ -56,6 +67,22 @@ const ViewEmpOrders = () => {
         }
     };
 
+    const handleUpdate = async(allowance_id, amount) => {
+        const data = {
+            allowance_status : '1',
+            paid_amount : amount,
+            balance_amount : 0
+        }
+        console.log(allowance_id, data)
+        try{
+            await updateAllowanceStatus(allowance_id, data)
+            window.location.reload()
+        }
+        catch(error){
+            console.log('error :', error)
+        }
+    }
+
     return (
         <div id='myOrders'>
             <div style={{ display: 'flex' }}>
@@ -73,6 +100,9 @@ const ViewEmpOrders = () => {
                                     <th>Status</th>
                                     <th>Task Acceptance</th>
                                     <th>Work Status</th>
+                                    <th>Allowance</th>
+                                    <th>Allowance Status</th>
+
                                 </tr>
                             </thead>
                             <tbody>
@@ -85,6 +115,15 @@ const ViewEmpOrders = () => {
                                         <td>{order.status}</td>
                                         <td>{order.task_acceptence !== null ? order.task_acceptence : 'N/A'}</td>
                                         <td className={addClassName(order.status)}>{order.work_status !== null ? order.work_status : 'N/A'}</td>
+                                        {allowanceDetails.length > 0 && allowanceDetails.map((allowance) => (
+                                            (order.order_id === allowance.order_id) && <td>{allowance.total_amount}</td>
+                                        ))
+                                        } 
+                                        {allowanceDetails.length > 0 && allowanceDetails.map((allowance) => (
+                                            (order.order_id === allowance.order_id) &&( (allowance.allowance_status) === '1' ?
+                                            <p style={{fontWeight:'bold', color:'green'}}>Completed</p> : <button className='btn btn-danger' onClick={() => handleUpdate(allowance.allowance_id, allowance.balance_amount)}>Complete</button> )
+                                        ))
+                                        } 
                                     </tr>
                                 ))}
                             </tbody>
